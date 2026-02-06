14dc197d0c61c206514e1f679a3767cbcec5226e
okan-can-nazli <okancanazli@gmail.com>
2026-02-06 21:35:29 +0300

    updated test file

--- simple_test_task_1.py
+++ simple_test_task_1.py
@@ -2,42 +2,52 @@
 from lstm import LSTMCell
 import numpy as np
 
-lstm = LSTMCell(input_size=1, stm_size=1)
+all_inputs_size_value = 100
+stm_size_value = 128
+loop_count = 100
 
-x_sequence = np.random.rand(100,1)
+lstm = LSTMCell(input_size=1, stm_size=stm_size_value)
 
-for step in range(5000):
-    stm_outputs, ltm_current = lstm.forward_sequence(x_sequence,stm_init=np.zeros((1,1)),ltm_init=np.zeros((1,1)))
-    stm_outputs = np.array(stm_outputs).reshape(-1, 1) #make it 2-d shape from a list to be able to make operation for all stm values
+x_sequence = np.random.rand(all_inputs_size_value,1)
 
-    stm_formatted = stm_outputs.reshape(100, 1)
+for step in range(loop_count):
+    stm_outputs, ltm_current = lstm.forward_sequence(x_sequence,stm_init=np.zeros((stm_size_value,1)),ltm_init=np.zeros((stm_size_value,1)))
+    
+    stm_array = np.array(stm_outputs)
+    stm_reshaped = stm_array.reshape(-1, 128)
+    stm_condensed = stm_reshaped.mean(axis=1)
+    stm_final = stm_condensed.reshape(-1, 1)
 
-    loss = ((x_sequence - stm_formatted) ** 2).mean() #find "All Squared Residuals" in a matrix shaped (100,1)
 
 
     #CHECK THİS PART FOR LATER IT DOESNT MAKE THAT SENSE FOR ME FOR NOW!!!!!
     #================================================================#
-    loss_gradient = 2 * (stm_formatted - x_sequence) / len(x_sequence)
     
-    loss_gradient = loss_gradient.reshape(100, 1, 1)
+    #(x_sequence - stm_final) ^ 2
+    dloss = 2 * (stm_final - x_sequence) / len(x_sequence)
     
+    
+    dloss_expanded = np.repeat(dloss, stm_size_value, axis=1) / stm_size_value
+    
+    
+    loss_gradient = dloss_expanded.reshape(-1, 1)    
     accumulated_grads = lstm.backward_sequence(loss_gradient)
     #================================================================#
 
 
 
 
-
-    lstm.update_weights(accumulated_grads,learning_rate=0.01)
-
-
+    lstm.update_weights(accumulated_grads,learning_rate=0.5)
+    
+    print(f"There are {loop_count - step} more loops left to finish!")
         
         
-        
-# Final Test Forward
-final_outputs, _ = lstm.forward_sequence(x_sequence, np.zeros((1,1)), np.zeros((1,1)))
-final_outputs = np.array(final_outputs).reshape(-1, 1)
+# final test forward
+final_outputs, _ = lstm.forward_sequence(x_sequence, np.zeros((stm_size_value,1)), np.zeros((stm_size_value,1)))
+
+final_array = np.array(final_outputs).reshape(-1, 128).mean(axis=1).reshape(-1, 1)
 
 print("\nInput vs Output:")
-for i, (inp, out) in enumerate(zip(x_sequence, final_outputs)):
+
+for i, (inp, out) in enumerate(zip(x_sequence, final_array)):
     print(f"{inp[0]:.1f} -> {out[0]:.4f}")
