39f2fb457ade1cbae69b035ed9669c24db889937
okan-can-nazli <okancanazli@gmail.com>
2026-02-14 22:41:38 +0300

    RNN Cell has done

--- LSTM/RNN/rnn.py
+++ LSTM/RNN/rnn.py
@@ -9,12 +9,12 @@ class RNNCell:
         self.input_size = input_size
         self.hidden_size = hidden_size
         
-        self.w1 = np.random.rand(input_size, hidden_size)
+        self.w1 = np.random.randn(input_size, hidden_size) * 0.01 
         self.b1 = np.zeros((1,hidden_size))
         
-        self.w2 = np.random.rand(hidden_size,hidden_size)
+        self.w2 = np.random.randn(hidden_size,hidden_size) * 0.01
 
-        self.w3 = np.random.rand(hidden_size,hidden_size)
+        self.w3 = np.random.randn(hidden_size,hidden_size) * 0.01
         self.b2 = np.zeros((1,hidden_size))
     
         #I could also initialize hidden_state however it is better to get it from user too
@@ -46,6 +46,7 @@ class RNNCell:
         self.func_out = func_out
         self.step_outputs = step_outputs
         
+        # pass vars to backward method 
         self.x = x
         self.h_prev = h_prev
         self.sum_input = sum_input
@@ -67,46 +68,45 @@ class RNNCell:
     
     def backward(self, d_all_outputs):
         
+        h_next = self.h_prev # h from next cell
+        
         d_out_next = d_all_outputs
         
         
-        # output = (ReLU(x * w1 + h_prev * w2 + b1)* w3) + b2
+        # output = (ReLU(x * w1 + h_prev * w2 + b1) * w3) + b2
         # apply ! THE CHAIN RULE !
         
-        dfunc = np.dot(d_out_next, self.w3.T)     #TRANSPOSE, HOW DİDNT U EVEN NOTİCE THAT?!  |    d_out_next * self.w3 . T  !!!!
+        dfunc = np.dot(d_out_next, self.w3.T) * (self.sum_input>0)    #TRANSPOSE, HOW DİDNT U EVEN NOTİCE THAT?!  
         
-        dx = np.dot(dfunc, self.w1.T)         #  dfunc * self.w2 . T
-        dh = np.dot(dfunc, self.w2.T)               #  dfunc * self.w2 . T
-        dw1 = np.dot(self.x.T,dfunc)         # dfunc * x
-        dw2 = np.dot(self.h_prev.T,dfunc)              # dfunc * h
+        dx = np.dot(dfunc, self.w1.T)        
+        dh_next = np.dot(dfunc, self.w2.T)        
+        dw1 = np.dot(self.x.T,dfunc)         
+        dw2 = np.dot(h_next.T,dfunc)             
         dw3 = np.dot(self.func_out.T,d_out_next)
-        db1 = (self.sum_input > 0) * dfunc
+        db1 = dfunc
         db2 = d_out_next
         
     
+        # TRANSPOSE RULE: not about math, about making shapes work
+        
+        # Forward:  np.dot(A, B)
+        # Backward: 
+        #           dA = np.dot(grad, B.T)  → B.T flips shape so result matches A
+        #           dB = np.dot(A.T, grad)  → A.T flips shape so result matches B
+        
+
+        lr = 0.01
+        self.w1 -= lr * dw1
+        self.w2 -= lr * dw2
+        self.w3 -= lr * dw3
+        self.b1 -= lr * db1
+        self.b2 -= lr * db2
+        
+        return dh_next
     
     
-    
-
-"""
-Simple rule:
-
-In forward pass: output = np.dot(A, B)
-
-In backward pass:
-
-Gradient for A = np.dot(grad, B.T)
-Gradient for B = np.dot(A.T, grad)
-Left side of dot → use right.T Right side of dot → use left.T
-
-
-
-step_outputs = np.dot(func_out, self.w3)
-
-func_out is left → gradient = np.dot(grad, self.w3.T) ✅ (your dfunc)
-self.w3 is right → gradient = np.dot(func_out.T, grad) (your dw3)
-
-sum_input = np.dot(x, self.w1)
-
-x is left → gradient = np.dot(dfunc, self.w1.T) ✅ (your dx)
-self.w1 is right → gradient = np.dot(x.T, dfunc) (your dw1)"""
+    def backward_sequence(self,d_outputs):
+        dh_next = np.zeros((1, self.hidden_size))
+        
+        for d_out in reversed(d_outputs):
+            dh_next = self.backward(d_out + dh_next)
